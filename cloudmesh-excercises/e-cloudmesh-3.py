8e681dfeaca7ef487cc27befed35651fd2ffec27
AKSHAY KOWSHIK <54605176+akshayrf12345@users.noreply.github.com>
2019-11-08 22:21:17 -0500

    Create e-cloudmesh-3.py

---
+++ cloudmesh-excercises/e-cloudmesh-3.py
@@ -0,0 +1,19 @@
+from cloudmesh.common.FlatDict import FlatDict
+from cloudmesh.common.Printer import Printer
+import pytest
+
+data = {
+    "name":"Akshay Kowshik",
+    "address": {
+        "city":"Bloomington",
+        "state":"Indiana",
+        "country":"United States"
+
+                }
+        }
+
+flat = FlatDict(data,sep='__')
+
+def table_test(self):
+    table = Printer.flatwrite(self.data,sort_keys=["name"],order=["name","address.city","address.state"],header=["Name","City","State"],output='table')
+    print(table)
