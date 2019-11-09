ae1144b84d5cfd3f0d209795c256d0343ef6d1f1
AKSHAY KOWSHIK <54605176+akshayrf12345@users.noreply.github.com>
2019-11-08 22:18:49 -0500

    Update e-cloudmesh-1.py

--- cloudmesh-excercises/e-cloudmesh-1.py
+++ cloudmesh-excercises/e-cloudmesh-1.py
@@ -4,4 +4,14 @@ Created on Fri Sep 13 12:13:50 2019
 
 @author: aksha
 """
+from cloudmesh.common.util import banner, HEADING
+from cloudmesh.common.debug import VERBOSE
 
+banner("This is my first print command",debug=True,label="Label for Banner",color="GREEN")
+
+banner("Trying out Heading")
+HEADING("This is interesting!",c='#',color='HEADER')
+
+banner("This is Verbose")
+months = {"January": "1", "February": "2", "March":"3","April":"4","May":"5","June":"6","July":"7","August":"8","September":"9","October":"10","November":"11","December":'12'}
+VERBOSE(d,label="Months",color='RED', verbose=-10)
